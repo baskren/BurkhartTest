af0e4092f5f2deaf881b75cef863f9335d03b965
Ben Askren <ben@buildcalc.com>
2016-11-09 12:06:34 -0500

    first commit

---
+++ ThomasBurkhart/Burkhart.xaml.cs
@@ -0,0 +1,20 @@
+﻿// /*******************************************************************
+//  *
+//  * Burkhart.xaml.cs copyright 2016 ben, 42nd Parallel - ALL RIGHTS RESERVED.
+//  *
+//  *******************************************************************/
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ThomasBurkhart
+{
+	public partial class Burkhart : ContentPage
+	{
+		public Burkhart()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ ThomasBurkhart/ThomasBurkhart.cs
@@ -0,0 +1,50 @@
+﻿// /*******************************************************************
+//  *
+//  * ThomasBurkhart.cs copyright 2016 ben, 42nd Parallel - ALL RIGHTS RESERVED.
+//  *
+//  *******************************************************************/
+using System;
+
+using Xamarin.Forms;
+
+namespace ThomasBurkhart
+{
+	public class App : Application
+	{
+		public App()
+		{
+			// The root page of your application
+			var content = new ContentPage
+			{
+				Title = "ThomasBurkhart",
+				Content = new StackLayout
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							HorizontalTextAlignment = TextAlignment.Center,
+							Text = "Welcome to Xamarin Forms!"
+						}
+					}
+				}
+			};
+
+			MainPage = new NavigationPage(content);
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
